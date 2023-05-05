5828638f5ac95972c0fbf845493a0c1b944e00a5
Alessio <113304040+NoBueno11@users.noreply.github.com>
2023-05-05 02:18:30 +0200

    Dice App
    This is my third app!

---
+++ ThirdApp/Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
+    var leftDiceNumber = 1
+    var rightDiceNumber = 5
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        let diceArray = [
+            UIImage(named: "DiceOne"),
+            UIImage(named: "DiceTwo"),
+            UIImage(named: "DiceThree"),
+            UIImage(named: "DiceFour"),
+            UIImage(named: "DiceFive"),
+            UIImage(named: "DiceSix")
+        ]
+        
+        diceImageViewOne.image = diceArray.randomElement()!
+        diceImageViewTwo.image = diceArray.randomElement()!
+        
+    }
+}
+
