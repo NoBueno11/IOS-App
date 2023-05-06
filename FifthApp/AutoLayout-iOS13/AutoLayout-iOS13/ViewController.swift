5df5e01c553a9e58146bf07369406fc5d40fac42
Alessio <113304040+NoBueno11@users.noreply.github.com>
2023-05-06 02:04:32 +0200

    AutoLayout App
    This is my fifth app

---
+++ FifthApp/AutoLayout-iOS13/AutoLayout-iOS13/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  AutoLayout-iOS13
+//
+//  Created by Angela Yu on 28/06/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        let allDice = [
+            UIImage(named: "DiceOne"),
+            UIImage(named: "DiceTwo"),
+            UIImage(named: "DiceThree"),
+            UIImage(named: "DiceFour"),
+            UIImage(named: "DiceFive"),
+            UIImage(named: "DiceSix"),
+        ]
+        
+        diceImageView1.image = allDice.randomElement()!
+        diceImageView2.image = allDice.randomElement()!
+        
+    }
+    
+}
+
