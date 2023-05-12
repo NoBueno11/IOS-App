dd3b2984456870bb7c2e2ffd36c7d4047297e26a
Alessio <113304040+NoBueno11@users.noreply.github.com>
2023-05-12 17:49:07 +0200

    BMI-Calculator App
    This is my eleventh app

---
+++ EleventhApp/BMI-Calculator-iOS13/BMI Calculator/Models/BMI.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  BMI Calculator
+//
+//  Created by Alessio on 5/10/23.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    
+    let value: Float
+    let advice: String
+    let color: UIColor
+    
+}
---
+++ EleventhApp/BMI-Calculator-iOS13/BMI Calculator/Models/CalculatorBrain.swift
@@ -0,0 +1,57 @@
+//
+//  CalculateBrain.swift
+//  BMI Calculator
+//
+//  Created by Alessio on 5/9/23.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct CalculatorBrain {
+    
+    var bmi: BMI?
+    
+    func getBMIValue() -> String {
+        
+        let bmiToDecimalPlace = String(format: "%.1f", bmi?.value ?? 0.0)
+        
+        return bmiToDecimalPlace
+        
+    }
+    
+    mutating func calculateBMI(height: Float, weight: Float) {
+        
+        let bmiValue = weight / (height * height)
+        
+        //The color literal can be added using the following string(hashtag and single parenthesis): #colorLiteral(
+        let color = (underweight: #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1), healthy: #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1), overweight: #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1))
+        
+        if bmiValue < 18.5 {
+            
+            bmi = BMI(value: bmiValue, advice: "Eat more pies!", color: color.underweight)
+            
+        } else if bmiValue < 24.9 {
+            
+            bmi = BMI(value: bmiValue, advice: "Fit as a fiddle!", color: color.healthy)
+            
+        } else {
+            
+            bmi = BMI(value: bmiValue, advice: "Eat less pies!", color: color.overweight)
+            
+        }
+    }
+    
+    func getAdvice() -> String {
+        
+        return bmi?.advice ?? "No advice!"
+        
+    }
+    
+    func getColor() -> UIColor {
+        
+        
+        return bmi?.color ?? UIColor.white
+        
+    }
+}
